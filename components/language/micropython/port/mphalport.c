41ee13170955bd46fa507a296074653412bedace
KYzhang_X <tjkdbd@163.com>
2022-07-28 11:37:56 +0800

    micropython: partly extra feature porting

--- components/language/micropython/port/mphalport.c
+++ components/language/micropython/port/mphalport.c
@@ -3,7 +3,8 @@
 #include "tos_hal.h"
 #include "mpshellport.h"
 
-int mp_hal_stdin_rx_chr(void) {
+int mp_hal_stdin_rx_chr(void) 
+{
     while (1) {
         int c = mp_shell_getchar();
         if (c != -1) {
@@ -13,7 +14,17 @@ int mp_hal_stdin_rx_chr(void) {
     }
 }
 
-void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) 
+{
     mp_shell_putsn(str, len);
 }
 
+mp_uint_t mp_hal_ticks_ms(void)
+{
+    return tos_tick2millisec(tos_systick_get());
+}
+
+void mp_hal_delay_ms(mp_uint_t ms)
+{
+    tos_task_delay(tos_millisec2tick(ms));
+}
